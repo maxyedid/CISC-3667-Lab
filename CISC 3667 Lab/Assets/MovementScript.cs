23573c6c1e39c90747b8733c0319d30ce9d654a6
Max Yedid <maxyedid1@gmail.com>
2023-04-04 19:08:16 -0400

    Added movement script for the archer sprite to move right and left, currently working on getting the sprite to jump

--- CISC 3667 Lab/Assets/MovementScript.cs
+++ CISC 3667 Lab/Assets/MovementScript.cs
@@ -6,6 +6,12 @@ public class MovementScript : MonoBehaviour
 {
 
     public Rigidbody2D myRigidBody;
+    public float speed = 10.0f;
+    public float movement;
+    public bool jumpPressed;
+    public float jumpForce = 500.0f;
+    public bool facingRight = true;
+    public bool isGrounded = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,34 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 90));
+        movement = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            jumpPressed = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        myRigidBody.velocity = new Vector2(speed * movement, myRigidBody.velocity.y);
+        if (movement < 0 && !facingRight || movement > 0 && facingRight) {
+            Flip();
+        }
+        if (jumpPressed && isGrounded) {
+            Jump();
+        } else {
+            jumpPressed = false;
+        }
+    }
+
+    private void Flip() {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+    private void Jump() {
+        myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, 0);
+        myRigidBody.AddForce(new Vector2(0, jumpForce));
+
+        jumpPressed = false;
+        isGrounded = false;
     }
 }
