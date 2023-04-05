f197d15df228c0f3e5e205a50929f18df021ac09
Max Yedid <maxyedid1@gmail.com>
2023-04-04 20:42:18 -0400

    Added jump mechanic for the player as well as the platform as the ground for the player. Also added walls to prevent the player from falling off the map

--- CISC 3667 Lab/Assets/MovementScript.cs
+++ CISC 3667 Lab/Assets/MovementScript.cs
@@ -6,12 +6,14 @@ public class MovementScript : MonoBehaviour
 {
 
     public Rigidbody2D myRigidBody;
-    public float speed = 10.0f;
+    public float speed = 7.0f;
     public float movement;
     public bool jumpPressed;
     public float jumpForce = 500.0f;
     public bool facingRight = true;
     public bool isGrounded = true;
+
+    public bool sprinting = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,14 +24,24 @@ public class MovementScript : MonoBehaviour
     void Update()
     {
         movement = Input.GetAxis("Horizontal");
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKey(KeyCode.Space)) {
             jumpPressed = true;
+        }
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            sprinting = true;
+        } else {
+            sprinting = false;
         }
     }
 
     private void FixedUpdate() {
+        if (sprinting) {
+            speed = 10;
+        } else {
+            speed = 7;
+        }
         myRigidBody.velocity = new Vector2(speed * movement, myRigidBody.velocity.y);
-        if (movement < 0 && !facingRight || movement > 0 && facingRight) {
+        if (movement < 0 && facingRight || movement > 0 && !facingRight) {
             Flip();
         }
         if (jumpPressed && isGrounded) {
@@ -50,5 +62,11 @@ public class MovementScript : MonoBehaviour
 
         jumpPressed = false;
         isGrounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag.Equals("Ground")) {
+            isGrounded = true;
+        }
     }
 }
