b491d2847151200865ebf59ff2ea1615dbd5543e
Max Yedid <maxyedid1@gmail.com>
2023-04-29 20:25:13 -0400

    Fixed the back button on the high score page, will be working to add some high scores to that page.

--- CISC 3667 Lab/Assets/Scripts/muteMusic.cs
+++ CISC 3667 Lab/Assets/Scripts/muteMusic.cs
@@ -8,13 +8,14 @@ public class muteMusic : MonoBehaviour
 
     public Toggle toggle;
     public AudioSource music;
-
     public GameObject offIcon;
 
     // Start is called before the first frame update
     void Start()
     {
         music = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+        toggle.isOn = !music.mute;
+        offIcon.SetActive(music.mute);
     }
 
     // Update is called once per frame
